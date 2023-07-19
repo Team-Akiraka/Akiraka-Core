d55772e6fda2c3d4384cf10f2eb9d03591a3eed3
arrokoth <arrokoth233@gmail.com>
2023-07-19 23:57:19 +0200

    Version info

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+pub struct VersionInfo {
+    version_name: String,
+    version_type: String
+}
+
+#[cfg(test)]
+mod test {
+    use crate::VersionInfo;
+
+    #[test]
+    fn test() {
+        let version_info = VersionInfo {
+            version_name: "1.20.1".parse().unwrap(),
+            version_type: "release".parse().unwrap()
+        };
+    }
+}
